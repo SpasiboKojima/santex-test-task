2b6b5bc16125b69ef9c033a81ec1b2d4bc92b892
SpasiboKojima <andrei.katsewich@yandex.ru>
2023-07-10 11:53:46 +0300

    Merged Controller with Resolver

--- src/Server/Module/Competition/Resolver.ts
+++ src/Server/Module/Competition/Resolver.ts
@@ -1,15 +1,67 @@
-import CompetitionController from '../../Controller/Competition'
+import { eq } from 'drizzle-orm'
+import { db } from 'src/db/db'
+import { competition, player, team } from 'src/db/schema/schema'
+import { returnSuccessResponse } from 'src/helpers/Response'
 
-const CompetitionQueries: any = {
+import { ApiCompetitionResponse, ApiCompetitionTeamsResponse } from './Types'
+
+interface ImportLeagueArgs {
+  leagueCode: string
+}
+
+interface TeamArgs {
+  name: string
+}
+
+const CompetitionQueries = {
   Query: {
-    players: CompetitionController.Queries.players,
-    team: CompetitionController.Queries.team,
+    players: async (_, { leagueCode }: ImportLeagueArgs) => {
+      const result = await db.select().from(player)
+
+      return result
+    },
+    team: async (_, { name }: TeamArgs) => {
+      const result = await db.select().from(team).where(eq(team.name, name))
+
+      return result
+    },
   },
 }
 
-const CompetitionMutations: any = {
+const CompetitionMutations = {
   Mutation: {
-    importLeague: CompetitionController.Mutations.importLeague,
+    importLeague: async (_, { leagueCode }: ImportLeagueArgs) => {
+      const competitionResponse: ApiCompetitionResponse = await fetch(`https://api.football-data.org/v4/competitions/${leagueCode}`, {
+        headers: [['X-Auth-Token', '']],
+      }).then((response) => response.json())
+
+      const competitionObj = { name: competitionResponse.name, code: competitionResponse.code, areaName: competitionResponse.area.name }
+      await db.insert(competition).values(competitionObj)
+
+      const teamsResponse: ApiCompetitionTeamsResponse = await fetch(`https://api.football-data.org/v4/competitions/${leagueCode}/teams`, {
+        headers: [['X-Auth-Token', '']],
+      }).then((response) => response.json())
+
+      const teams = teamsResponse.teams.map((team) => ({
+        name: team.name,
+        tla: team.tla,
+        shortName: team.shortName,
+        areaName: team.area.name,
+        address: team.address,
+        players: team.squad.map((player) => ({
+          name: player.name,
+          position: player.position,
+          dateOfBirth: player.dateOfBirth,
+          nationality: player.nationality,
+        })),
+      }))
+      await db.insert(team).values(teams)
+
+      return returnSuccessResponse({
+        competition: competitionObj,
+        teams,
+      })
+    },
   },
 }
 
